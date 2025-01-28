0b5c6e0b54eb11e38aef29b7fe02657fe2255a66
shivajikhot <shivajkv@gmail.com>
2025-01-28 21:11:56 +0530

    Update variables.tf

--- environments/development/variables.tf
+++ environments/development/variables.tf
@@ -1,3 +1,7 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+}
 variable "cidr_block" {
   description = "CIDR block for the VPC"
   type        = string
